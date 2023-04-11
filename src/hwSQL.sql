a4b08343782a2ad6d9ab7bc10973cd38335ad9de
Nikitachinko <118566901+Nikitachinko@users.noreply.github.com>
2023-04-11 16:43:20 +0300

    Выполнил ДЗ 2

--- src/hwSQL.sql
+++ src/hwSQL.sql
@@ -22,3 +22,35 @@ UPDATE employee SET first_name = 'Roman', last_name = 'Gerasimov', age = 20 WHER
 SELECT * FROM employee;
 DELETE FROM employee WHERE id = 1;
 SELECT * FROM employee;
+INSERT INTO employee (first_name, last_name, gender, age)
+VALUES ('Ivan', 'Ivanov', 'male', 19);
+INSERT INTO employee (first_name, last_name, gender, age)
+VALUES ('Petr', 'Rakov', 'male', 33);
+INSERT INTO employee (first_name, last_name, gender, age)
+VALUES ('Zlata', 'Kong', 'female', 18);
+SELECT *
+FROM employee;
+SELECT first_name AS Имя, last_name AS Фамилия
+FROM employee;
+SELECT * FROM employee
+WHERE age < 30
+   OR age > 50;
+SELECT * FROM employee
+WHERE age BETWEEN 30 and 50;
+SELECT * FROM employee
+ORDER BY last_name DESC;
+SELECT * FROM employee
+WHERE LENGTH(first_name) > 4;
+UPDATE employee
+SET first_name = 'Egor'
+WHERE id = 5;
+UPDATE employee
+SET first_name = 'Kate'
+WHERE id = 6;
+SELECT first_name AS Имя, SUM(age) AS Суммарный_возраст FROM employee
+GROUP BY Имя;
+SELECT first_name, age FROM employee
+WHERE age = (SELECT MIN(age) FROM employee);
+SELECT first_name AS имя, MAX(age) AS максимальный_возраст FROM employee
+GROUP BY first_name
+HAVING COUNT(first_name) > 1;
